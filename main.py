465787a273b85fb7436ef099cbe25509d8676039
linghong <linghong_chen@hotmail.com>
2023-09-26 16:21:46 -0400

    Implement CORS and api_key-based Authentication

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from datetime import timedelta
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import  Header, HTTPException, Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
 
 from config import *
 from models import Token, User
 from dependencies import *
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+load_dotenv()  # Load environment variables from .env file
+API_KEY = os.getenv("API_KEY")
+
+async def get_api_key(api_key: str = Header(...)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    return api_key
 
 @app.post("/token", response_model=Token)
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
@@ -33,3 +52,7 @@ async def root(current_user: User = Depends(get_current_active_user)):
     if current_user:
         return {"message": "Hello World"}
     return {"message": "Unauthorized"}
+
+@app.get("/v1/api/welcome")
+async def third_party(api_key: str = Depends(get_api_key)):
+    return {"msg": "This is an authenticated route for external app", "api_key": api_key}
