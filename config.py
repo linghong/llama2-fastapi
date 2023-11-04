cbb5079f11df8917d7957a7e909954ebcfd2ccfb
linghong <linghong_chen@hotmail.com>
2023-11-03 23:54:46 -0400

    .env.example file

--- config.py
+++ config.py
@@ -13,6 +13,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 # key used for send data to the API end points
 API_SECRET_KEY = os.getenv("API_SECRET_KEY")
 
+# IP address that host the Client site or site url:
+YOUR_CLIENT_SITE_ADDRESS = os.getenv("YOUR_CLIENT_SITE_ADDRESS")
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
